fd7e60809701f24b95e64aa2b8aa120050c80139
Jimmy <jgdames@gmail.com>
2024-01-10 21:46:34 -0600

    Add PrivateKeyFileWithPassphrase from #11

--- private_key_file.go
+++ private_key_file.go
@@ -6,13 +6,28 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-func PrivateKeyFile(file string) ssh.AuthMethod {
+func PrivateKeyFile(file string, pass ...string) ssh.AuthMethod {
 	buffer, err := os.ReadFile(file)
 	if err != nil {
 		return nil
 	}
 
 	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+
+	return ssh.PublicKeys(key)
+}
+
+// All credits go to the author fagnercarvalho
+func PrivateKeyFileWithPassphrase(file string, passphrase string) ssh.AuthMethod {
+	buffer, err := os.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKeyWithPassphrase(buffer, []byte(passphrase))
 	if err != nil {
 		return nil
 	}
